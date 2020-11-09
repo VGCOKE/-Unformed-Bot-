6a3d404ab1705fd948b70b6d0a26ae211ef96f6e
Human666 <68257361+VGCOKE@users.noreply.github.com>
2020-11-09 18:43:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-helloconst Discord = require('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { prefix, bot_info, token } = require('./config.json');
+client.once('ready', () => {
+	console.log(bot_info.name);
+	console.log(bot_info.prefix);
+});
+
+client.login(token);
+
+client.on('message', message => {
+	if(message.content === `${prefix}help`) {
+		message.channel.send('Hi! I am **Eh Bot**! Use  / for commands! ');
+	}else if(message.content === `${prefix}member`) {
+		message.channel.send(`Total Members : **${message.guild.memberCount}**`);
+	}else if(message.content === `${prefix}server`) {
+		message.channel.send(`Server Name : **${message.guild.name}**`);
+	}else if(message.content === `${prefix}info`) {
+		message.channel.send(`Username : **${message.author.username}**`);
+		message.channel.send(`ID : **${message.author.id}**`);
+	}
+});
